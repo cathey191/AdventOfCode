85a82d9c5a8566efbb469ff626c105838b0b04b0
Catherine Clague <catherine.clague@trademe.co.nz>
2021-12-07 11:59:43 +1300

    Day 3, part 1, completed

--- day3/Day3.cs
+++ day3/Day3.cs
@@ -4,17 +4,43 @@ class Day3
 {
     static void Main()
     {
-        var logFile = File.ReadAllLines(@"input.txt");
-        var logList = new List<string>(logFile);
+        string[] input = System.IO.File.ReadAllLines(@"input.txt");
+        string gamma = "";
+        string epsilon = "";
 
-        for (int i = 0; i < binaryInp[0].Length; i++)
+        for (int i = 0; i < input[0].Length; i++)
         {
-            for (int j = 0; j < binaryInp.Length; j++)
+            int zeroCount = 0;
+            int oneCount = 0;
+            
+            for (int j = 0; j < input.Length; j++)
             {
-                
+                if (input[j][i].Equals('0'))
+                {
+                    zeroCount++;
+                }
+                else
+                {
+                    oneCount++;
+                }
+            }
+
+            if (zeroCount > oneCount)
+            {
+                gamma = gamma + "0";
+                epsilon = epsilon + "1";
+            }
+            else
+            {
+                epsilon = epsilon + "0";
+                gamma = gamma + "1";
             }
         }
+
+        int gammaInt = Convert.ToInt32(gamma, 2);
+        int epsilonInt = Convert.ToInt32(epsilon, 2);
+        int result = gammaInt * epsilonInt;
         
-        System.Console.WriteLine("Result = " + logList);
+        System.Console.WriteLine("Result = " + result);
     }
 }
