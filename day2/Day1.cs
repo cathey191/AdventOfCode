cb0e37faec242faac3986589cbc05bc919237903
Catherine Clague <catherine.clague@trademe.co.nz>
2021-12-07 11:21:58 +1300

    Day 2, part 2, complete

--- day2/Day1.cs
+++ day2/Day1.cs
@@ -7,6 +7,7 @@ class Day2
         string[] input = System.IO.File.ReadAllLines(@"input.txt");
         int hor = 0;
         int depth = 0;
+        int aim = 0;
 
         for (int i = 0; i < input.Length; i++)
         {
@@ -14,16 +15,17 @@ class Day2
             {
                 int num = input[i][input[i].Length - 1] - '0';
                 hor = hor + num;
+                depth = depth + aim * num;
             } 
             else if (input[i].Contains("down"))
             {
                 int num = input[i][input[i].Length - 1] - '0';
-                depth = depth + num;
+                aim = aim + num;
             }
             else if (input[i].Contains("up"))
             {
                 int num = input[i][input[i].Length - 1] - '0';
-                depth = depth - num;
+                aim = aim - num;
             }
         }
 
