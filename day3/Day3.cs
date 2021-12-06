6fda5a4298266e351f3ed04215ac37aff0f59bbd
Catherine Clague <catherine.clague@trademe.co.nz>
2021-12-07 11:32:26 +1300

    Day 2, part 2, complete

---
+++ day3/Day3.cs
@@ -0,0 +1,20 @@
+using System;
+
+class Day3
+{
+    static void Main()
+    {
+        var logFile = File.ReadAllLines(@"input.txt");
+        var logList = new List<string>(logFile);
+
+        for (int i = 0; i < binaryInp[0].Length; i++)
+        {
+            for (int j = 0; j < binaryInp.Length; j++)
+            {
+                
+            }
+        }
+        
+        System.Console.WriteLine("Result = " + logList);
+    }
+}
